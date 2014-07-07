812f9dcac160db6be61fcd65e1d0014d9b40f11d
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-07-07 15:01:00 -0400

    Add ability to add feature in UI
    Conflicts:
    	lib/rollout_ui/engine/app/controllers/rollout_ui/features_controller.rb
    	lib/rollout_ui/engine/config/routes.rb
    	spec/requests/engine/engine_spec.rb

--- lib/rollout_ui/engine/config/routes.rb
+++ lib/rollout_ui/engine/config/routes.rb
@@ -1,5 +1,5 @@
 RolloutUi::Engine.routes.draw do
-  resources :features, :only => [:index, :update, :destroy] do
+  resources :features, :only => [:index, :create, :update, :destroy] do
     resource :current_user, :only => [:create, :destroy], :controller => 'features/current_users'
   end
 
